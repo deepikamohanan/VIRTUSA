591418515de0ecd1b59ec3d7cf75e75b47592651
Deepika <69636370+deepikamohanan@users.noreply.github.com>
2023-06-27 12:59:01 +0530

    Delete src/main/java/com/react/reactfullstack/exception directory

--- src/main/java/com/react/reactfullstack/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.react.reactfullstack.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id){
-        super("Could not found the user the user with id "+ id);
-    }
-}

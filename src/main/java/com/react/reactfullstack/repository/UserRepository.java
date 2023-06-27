f1a2b7b003c22d427f81f00b02bc1e935887128a
Deepika <69636370+deepikamohanan@users.noreply.github.com>
2023-06-27 12:59:11 +0530

    Delete src/main/java/com/react/reactfullstack directory

--- src/main/java/com/react/reactfullstack/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.react.reactfullstack.repository;
-
-import com.react.reactfullstack.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository <User,Long>{
-}
